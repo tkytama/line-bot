1a29154b7777467ab39e195bcd0e8afe9ff652e9
EC2 Default User <ec2-user@ip-172-31-23-92.us-east-2.compute.internal>
2019-11-30 10:48:48 +0000

    first commit

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module LineBot
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+  end
+end
